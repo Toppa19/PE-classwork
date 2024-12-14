7513ae282d2a3c6d8d385a7df1f2bd3b11e82bfc
unknown <olga.kvasok@gmail.com>
2024-12-14 12:33:54 +0000

    lesson 2 js

---
+++ basic_js/lesson2_JS-variables-basic-types/index.js
@@ -0,0 +1,23 @@
+var name = "Axen, Alexandr, Misha, Alex, Bogdan"; // string
+
+let text = "This variable has been created with let"; //string
+
+let emptyVariable; // undefined
+
+emptyVariable = name; // "Axen, Alexandr, Misha, Alex, Bogdan"
+emptyVariable = text;
+
+const kebabKing = "I'm kebab king";
+
+let number = -12; //number : 1,2,3,4,5,6,7,8,9,0
+
+const booleanTrue = true; //boolean
+const booleanFalse = false; //boolean
+
+console.log(name);
+console.log(text);
+console.log(emptyVariable);
+console.log(kebabKing);
+console.log(number);
+console.log(booleanTrue);
+console.log(booleanFalse);
